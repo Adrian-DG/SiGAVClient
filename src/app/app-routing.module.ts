81cec3fd7f9fb38608579647ad71241124bfac1d
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2023-03-27 15:39:47 -0400

    auth page worjing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthModule } from './modules/auth/auth.module';
+
+const routes: Routes = [
+	{
+		path: 'auth',
+		loadChildren: () =>
+			import('./modules/auth/auth.module').then((m) => m.AuthModule),
+	},
+	{ path: '', redirectTo: 'auth', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
