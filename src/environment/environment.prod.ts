ada3b7b89240fbfbf4acebf093142f9e27069c11
Adrian-DG <adrian96guilamo@gmail.com>
2023-04-27 10:09:40 +0800

    minor changes

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	api_url: 'https://asistenciavial.azurewebsites.net/api',
+	api_url: 'http://94.74.67.195:8080/api',
 	production: true,
 };
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-	api_url: 'https://localhost:7234/api',
+	api_url: 'http://localhost:5234/api',
 	production: false,
 };
