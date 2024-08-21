a65c647aa226cf307908613e487651835d592e8c
Adrian-DG <adrian96guilamo@gmail.com>
2024-08-20 22:52:49 -0400

    menu modules restrictions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,14 @@ import { AuthService } from './modules/auth/services/auth/auth.service';
 import { IUserData } from './modules/auth/interfaces/iuser-data';
 import { SpinnerService } from './modules/generic/services/spinner/spinner.service';
 
+export enum RolUser {
+	ANALISTA_OPERACIONES = 1,
+	OPERADOR_R5,
+	GESTION_OPERATIVA,
+	CALIDAD,
+	PREHOSPITALARIA,
+}
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
@@ -21,6 +29,8 @@ export class AppComponent implements OnInit {
 	];
 
 	constructor(public _auth: AuthService, public _spinner: SpinnerService) {}
+
+	public Roles = RolUser;
 
 	ngOnInit(): void {
 		this._auth.checkIfAuthenticated();
